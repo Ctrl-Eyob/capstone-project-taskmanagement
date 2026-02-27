913b577e7573bbbecced598277968b8ee71ae24d
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-02-27 12:12:35 +0300

    created signals.py added to auto create profiles

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, RegisterView
+from .views import (
+    TaskViewSet,
+    RegisterView,
+    CategoryViewSet,
+    ProfileViewSet
+)
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='tasks')
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'profiles', ProfileViewSet, basename='profiles')
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
